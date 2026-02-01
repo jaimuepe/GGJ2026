bf07bbf3432de45f0c16bcb5beada588a1bc1166
Jaime Muela Pérez <jaimuepe@gmail.com>
2026-02-01 17:22:20 +0100

    intro dont destroy on load

---
+++ Assets/Scripts/Masks/DontDestroyOnLoad.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+using System;
+using UnityEngine;
+
+namespace Masks
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
